cde360428cb9c1927271dd5d2d00f5deb7d8b8ae
Devon Neill <dev@neillfam.co.za>
2021-04-24 22:22:39 +0200

    Update Layout

---
+++ src/components/Layout/index.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { FaAngleDoubleLeft } from "react-icons/fa";
+import logo from "../../assets/images/logo.png";
+import GlobalStyles from "../../styles/GlobalStyles";
+import Typography from "../../styles/Typography";
+import styled from "styled-components";
+import { useHistory, useLocation } from "react-router-dom";
+
+const SiteWrapper = styled.div`
+  max-width: 1000px;
+  margin: 0.5rem auto 0.5rem auto;
+  background: white;
+  padding: 5px;
+  padding: clamp(5px, 1vw, 25px);
+  @media (max-width: 1100px) {
+    margin-left: 1.5em;
+    margin-right: 1.5em;
+  }
+`;
+
+const ContentStyles = styled.div`
+  background: white;
+  padding: 1rem;
+  text-align: center;
+`;
+
+const StyledHeader = styled.header`
+  height: clamp(40px, 3rem, 100px);
+  padding: 2rem 1rem 1rem 1rem;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  align-items: flex-start;
+  border-bottom: var(--grey) solid 2px;
+`;
+
+const StyledLink = styled.a`
+  &:hover {
+    color: var(--orange);
+    cursor: pointer;
+  }
+`;
+
+const Layout = ({ children }) => {
+  let history = useHistory();
+  let location = useLocation();
+  const onHomePage = () => {
+    return location.pathname === "/";
+  };
+
+  const handleClick = () => {
+    history.goBack();
+  };
+  return (
+    <>
+      <GlobalStyles />
+      <Typography />
+      <StyledHeader>
+        {!onHomePage() && (
+          <StyledLink onClick={handleClick}>
+            <FaAngleDoubleLeft />
+            Back
+          </StyledLink>
+        )}
+        <img src={logo} alt="logo" />
+      </StyledHeader>
+      <SiteWrapper>
+        <ContentStyles>{children}</ContentStyles>
+      </SiteWrapper>
+    </>
+  );
+};
+
+export default Layout;
